1d446c0adbb404613fb6432c531283aa04004252
Satya <satykrishna@gmail.com>
2017-05-19 23:29:56 -0400

    let & const introduction

---
+++ let&const/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+let color = 'red'; //This variable is expected to change
+const REFERENCE_COLOR = 'yellow'; //This variable doesn't change in its life time.
+
+module.exports = { color }
